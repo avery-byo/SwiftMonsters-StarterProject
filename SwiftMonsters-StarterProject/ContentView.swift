ae07ac3687687b7c8fd8486cce760e35bcd5a70d
Viona <165232192+avery-byo@users.noreply.github.com>
2025-05-08 09:19:42 +1000

    Update ContentView.swift

--- SwiftMonsters-StarterProject/ContentView.swift
+++ SwiftMonsters-StarterProject/ContentView.swift
@@ -1,21 +1,120 @@
 //
 //  ContentView.swift
-//  SwiftMonsters-StarterProject
+//  SwiftMonsters
 //
-//  Created by Allicia Viona Sagi on 07/05/25.
+//  Created by Allicia Viona Sagi on 06/05/25.
 //
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Image("Placeholder")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 250)
+            Text("Placeholder")
+                .font(.headline)
+            
+            VStack {
+                HStack {
+                    Text("__")
+                        .frame(width: 50)
+                    
+                    ProgressView(value: 0.5)
+                }
+                HStack {
+                    Text("__")
+                        .frame(width: 50)
+                    
+                    ProgressView(value: 0.8)
+                }
+            }
+            .padding()
+            
+            HStack {
+                Button {
+                    print("Placeholder")
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 25)
+                            .frame( height: 50)
+                        Text("Placeholder")
+                            .foregroundStyle(.white)
+                    }
+                }
+                
+                Button {
+                    print("Placeholder")
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(.red)
+                            .frame(height: 50)
+                        Text("Placeholder")
+                            .foregroundStyle(.white)
+                    }
+                }
+            }
+            
         }
         .padding()
+        
+        List {
+            Section(header: Text("Placeholder List")) {
+                
+                // Monster 1
+                HStack {
+                    Image("Placeholder")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                    Text("Placeholder")
+                }
+                
+                // Monster 2
+                HStack {
+                    Image("Placeholder")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                    Text("Placeholder")
+                }
+                
+                // Monster 3
+                HStack {
+                    Image("Placeholder")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                    Text("Placeholder")
+                }
+                
+                // Monster 4
+                HStack {
+                    Image("Placeholder")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                    Text("Placeholder")
+                }
+                
+                // Monster 5
+                HStack {
+                    Image("Placeholder")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                    Text("Placeholder")
+                }
+            }
+            
+        }
     }
 }
 
