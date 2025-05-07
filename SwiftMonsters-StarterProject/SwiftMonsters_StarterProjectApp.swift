6ec67329245afb57a2d9939c2fe381e11a1970f3
Allicia Viona <alliciawn@gmail.com>
2025-05-07 16:10:20 +0700

    Initial Commit

---
+++ SwiftMonsters-StarterProject/SwiftMonsters_StarterProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMonsters_StarterProjectApp.swift
+//  SwiftMonsters-StarterProject
+//
+//  Created by Allicia Viona Sagi on 07/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMonsters_StarterProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
